9b20298ad84c19da589b16ba7ee0aad6d04e28c8
Lars Schwegmann <schwegmannlars@gmail.com>
2019-06-11 13:25:16 +0200

    REfactor DHT Command to its own file

---
+++ Sources/dht-module/DHTCommand.swift
@@ -0,0 +1,31 @@
+//
+//  DHTCommand.swift
+//  CNIOAtomics
+//
+//  Created by Lars Schwegmann on 11.06.19.
+//
+
+import Foundation
+import SwiftCLI
+
+class DHTCommand: Command {
+
+    let configKey = Key<String>("-c", "--config", description: "Path to a custom config file")
+
+    func execute() throws {
+        guard let configPath = configKey.value else {
+            stderr <<< "Error: You need to specify a config file with -c"
+            return
+        }
+
+        guard let config = try Configuration(filePath: configPath) else {
+            fatalError("Loading the config from the config file at \(configPath) failed")
+        }
+        stdout <<< config.apiAddress
+    }
+
+    var name: String {
+        return "dht-module"
+    }
+
+}
--- Sources/dht-module/main.swift
+++ Sources/dht-module/main.swift
@@ -1,27 +1,6 @@
 import Foundation
 import SwiftCLI
 
-class DHTCommand: Command {
-
-    let configKey = Key<String>("-c", "--config", description: "Path to a custom config file")
-
-    func execute() throws {
-        guard let configPath = configKey.value else {
-            return
-        }
-
-        guard let config = try Configuration(filePath: configPath) else {
-            fatalError("Loading the config from the config file at \(configPath) failed")
-        }
-        stdout <<< config.apiAddress
-    }
-
-    var name: String {
-        return "dht-module"
-    }
-
-}
-
 let cli = CLI(singleCommand: DHTCommand())
 cli.goAndExit()
 
