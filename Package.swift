8e30ba20dfb29b073e1a89ff9744253a906c12c2
Dimitri Tyan <dimitri.tyan@gmail.com>
2019-06-12 18:14:22 +0200

    Update Swift Package and add Identifier file

--- Package.swift
+++ Package.swift
@@ -6,12 +6,14 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/apple/swift-nio.git", from: "2.0.0"),
         .package(url: "https://github.com/apple/swift-nio-extras.git", from: "1.0.0"),
-        .package(url: "https://github.com/larsschwegmann/SwiftCLI.git", .branch("master"))
+        .package(url: "https://github.com/larsschwegmann/SwiftCLI.git", .branch("master")),
+        .package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", .upToNextMinor(from: "1.0.0")),
+        .package(url: "https://github.com/larsschwegmann/UInt256.git", .branch("master"))
     ],
     targets: [
         .target(
             name: "DHTSwift",
-            dependencies: ["NIO", "NIOExtras", "SwiftCLI"]),
+            dependencies: ["NIO", "NIOExtras", "SwiftCLI", "CryptoSwift", "UInt256"]),
         .target(
             name: "DHTSwiftExecutable",
             dependencies: ["DHTSwift"]),
--- Sources/DHTSwift/Storage/Identifier.swift
+++ Sources/DHTSwift/Storage/Identifier.swift
@@ -7,6 +7,6 @@
 
 import Foundation
 
-class Identifer {
-    
+class Identifier {
+
 }
