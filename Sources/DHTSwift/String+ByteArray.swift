c8cbf5bcdb407ac52ee9b64d5cbd89c445c9100f
Lars Schwegmann <schwegmannlars@gmail.com>
2019-06-12 10:43:51 +0200

    Fix key byte conversion

--- Sources/DHTSwift/String+ByteArray.swift
+++ Sources/DHTSwift/String+ByteArray.swift
@@ -8,13 +8,34 @@
 import Foundation
 
 extension String {
-    func toByteArray() -> [UInt8] {
+    func toByteArray(cut: Int) -> [UInt8] {
+        if self.utf8.count == cut {
+            return Array(self.utf8)
+        } else if self.utf8.count > cut {
+            var bytes = [UInt8]()
+            for (i, byte) in self.utf8.enumerated() {
+                if i < cut {
+                    bytes[i] = byte
+                }
+            }
+            return bytes
+        } else if self.utf8.count < cut {
+            var bytes = Array(self.utf8)
+            (0...(cut - self.utf8.count)).forEach { _ in
+                bytes.append(0x00)
+            }
+            return bytes
+
+        }
         return Array(self.utf8)
     }
 }
 
 extension Array where Element == UInt8 {
-    func toString() -> String? {
+    func toString(size: Int) -> String? {
+        guard self.count == size else {
+            return nil
+        }
         return String(bytes: self, encoding: .utf8)
     }
 }
